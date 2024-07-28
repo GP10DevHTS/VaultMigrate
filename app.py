cfaff5d5549237e5edaecd8bdd4fd0553b9205dc
gp10devhts <jordankatetegirwe@gmail.com>
2024-07-28 04:50:34 +0300

    comments

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from tkinter import *
 
-
+# main window
 root = Tk()
 
+# welcome text | sample label
 welcomeLabel = Label(root, text="Welcome to VaultMigrate")
+#  add label to window
 welcomeLabel.pack()
 
+# main loop
 root.mainloop()
